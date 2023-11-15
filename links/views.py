a05720e8e21ea70ef1c88f8270dc106563246f0b
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-11-15 13:33:40 +0000

    improved the form use and added a redirect from after submitting form.

--- links/views.py
+++ links/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 from .models import Link
+from .forms import LinkForm
 # Create your views here.
 
 def index(request):
@@ -18,5 +20,14 @@ def root_link(request, link_slug):
     return redirect(link.url)
 
 def add_link(request):
-    print(request.POST)
-    return render(request, "links/create.html", {})
+    if request.method == 'POST':
+        form = LinkForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('home'))
+    else:
+        form = LinkForm()
+    context = {
+        'form': form
+    }
+    return render(request, "links/create.html", context)
