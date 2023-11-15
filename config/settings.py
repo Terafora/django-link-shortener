80dfe5b94a9ac775e7550847139241516625981f
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-11-15 14:02:43 +0000

    fixed styling and create link with some help from crispy modules.

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    #3rd party
+
+    "crispy_forms",
+    "crispy_tailwind",
+
     #local apps
 
     'links',
@@ -125,3 +130,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+
+CRISPY_TEMPLATE_PACK = "tailwind"
